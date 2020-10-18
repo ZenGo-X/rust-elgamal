3ed92053aa651605ab73e65adb247ded5d2db8c7
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2020-10-18 14:14:54 -0500

    update solvers

--- src/dl_solvers.rs
+++ src/dl_solvers.rs
@@ -4,6 +4,12 @@ use curv::BigInt;
 // see https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm_for_logarithms#:~:text=Pollard's%20rho%20algorithm%20for%20logarithms%20is%20an%20algorithm%20introduced%20by,solve%20the%20integer%20factorization%20problem.&text=of%20the%20equation-,.,using%20the%20Extended%20Euclidean%20algorithm
 
 const POLLARD_PARTITION_N: u32 = 3;
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum SolverError {
+    PollardConvergenceError,
+}
+
 pub struct SimplePollard<'a> {
     p: &'a BigInt,     // prime
     big_p: &'a BigInt, // prime + 1
@@ -39,7 +45,7 @@ impl<'a> SimplePollard<'a> {
         };
     }
 
-    pub fn run(&self) -> BigInt {
+    pub fn run(&self) -> Result<BigInt, SolverError> {
         let mut x = BigInt::one();
         let mut a = BigInt::zero();
         let mut b = BigInt::zero();
@@ -56,13 +62,22 @@ impl<'a> SimplePollard<'a> {
                 break;
             }
         }
-        x
+        if b.modulus(&self.p) == big_b.modulus(&self.p) {
+            return Err(SolverError::PollardConvergenceError);
+        }
+        Ok(x)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{
+        rfc7919_groups::SupportedGroups, ElGamalCiphertext, ElGamalKeyPair, ElGamalPP,
+        ExponentElGamal,
+    };
+    use curv::arithmetic::traits::Samplable;
+
     #[test]
     fn test_pollard() {
         let simple_pollard = SimplePollard {
@@ -72,6 +87,59 @@ mod tests {
             beta: &BigInt::from(5),
         };
         let res = simple_pollard.run();
-        assert_eq!(res, BigInt::from(1010));
+        assert!(res.is_ok());
+        let m = res.unwrap();
+        assert_eq!(m, BigInt::from(1010));
+    }
+
+    #[test]
+    fn test_convergence_error() {
+        fn eval(x1: &BigInt, x2: &BigInt, p: &BigInt) -> Result<(), SolverError> {
+            if x1.modulus(&p) == x2.modulus(&p) {
+                return Err(SolverError::PollardConvergenceError);
+            }
+            Ok(())
+        }
+        let p = BigInt::from(5);
+        let x11 = BigInt::from(6);
+        let x21 = BigInt::from(21);
+        let x22 = BigInt::from(22);
+
+        let get_err = eval(&x11, &x21, &p);
+        assert!(get_err.is_err());
+
+        let get_err = eval(&x11, &x22, &p);
+        assert!(get_err.is_ok());
+    }
+
+    // #[test]
+    fn test_exponent_elgamal_homomorphic_add_with_decryption() {
+        let group_id = SupportedGroups::FFDHE2048;
+        let pp = ElGamalPP::generate_from_rfc7919(group_id);
+        let keypair = ElGamalKeyPair::generate(&pp);
+        let message1 = BigInt::from(1_000);
+        let random1 = BigInt::sample_below(&pp.q);
+        let c1 =
+            ExponentElGamal::encrypt_from_predefined_randomness(&message1, &keypair.pk, &random1)
+                .unwrap();
+        let message2 = BigInt::from(3);
+        let random2 = BigInt::sample_below(&pp.q);
+        let c2 =
+            ExponentElGamal::encrypt_from_predefined_randomness(&message2, &keypair.pk, &random2)
+                .unwrap();
+        let c = ExponentElGamal::add(&c1, &c2).unwrap();
+
+        // let m = ExponentElGamal::decrypt(&c, &keypair.sk).unwrap();
+        // assert_eq!(BigInt::from(1_003), m);
+
+        let big_p = c.pp.p.clone() + BigInt::one();
+        let m = ExponentElGamal::decrypt_exp(&c, &keypair.sk).unwrap();
+
+        let simple_pollard = SimplePollard::new(&c.pp.p, &big_p, &c.pp.g, &m);
+        let res = simple_pollard.run();
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res, BigInt::from(1003));
+        // return Err(ElGamalError::DecryptionError);
     }
 }
